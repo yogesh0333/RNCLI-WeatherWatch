17e89454bf63fceb4c7f004199be534083d7a2d0
Dan Hannigan <dan.hannigan@bb2bank.com>
2024-08-01 16:02:03 +0100

    Update App and tests

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,13 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
 
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
+import {render, screen} from '@testing-library/react-native';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+it('should render the App', () => {
+  render(<App />);
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+  expect(
+    screen.getByText('Open up App.tsx to start working on your app!'),
+  ).toBeTruthy();
 });
